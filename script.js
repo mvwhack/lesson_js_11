6033e4b44b51cfb7b40260ec11021673c1946f1b
Vlad Mansurov <m_v2709@mail.ru>
2022-06-11 14:57:35 +0300

    Выполнил 11 задание

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+'use strict';
 
+const lesson11 = function () {
+  const btn = document.getElementById('btn');
+  const input = document.getElementById('text');
+  const square = document.getElementById('square');
+  const btnDispNone = document.getElementById('e_btn');
+  const inputRange = document.getElementById('range');
+  const circle = document.getElementById('circle');
 
+  btn.addEventListener("click", function () {
+    let val = input.value;
+    square.style.backgroundColor = val;
+    input.value = "";
+  });
 
+  btnDispNone.style.display = "none";
 
+  inputRange.addEventListener("input", function () {
+    let val = inputRange.value;
+    circle.style.width = val + '%';
+    circle.style.height = val + '%';
 
+    console.log(val);
+  });
+};
+
+lesson11();
